20bec8239f5aa7d1a9009fb087edd7ba171ef37e
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-03-29 01:32:23 +0300

    Create a basic Flutter Application.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Flutter Codelabs.
+
+This is my attempt at following the Flutter Documentation to create a Flutter App. This is my First Codelab. This app generates a list of StartUp Names for you from the Flutter English packages.
+
+### What I have Learnt.
+
+- How to get add packages to my app.
+- Difference between Stateless and Stateful Widgets.
+
+### To Run the Application:
+
+- Clone ther repository.
+- Ensure you have Flutter Installed.
+- Navigate to the location to which you cloned the repository.
+- Open the cloned repository using your terminal.
+- Start an Android emulator.
+- Run `flutter run`.
+
+`It's all Widgets.`
+Happy Coding!
+
+### Acknowledgements.
+
+- Google Codelabs.
+
+### Author.
+
+- Kelyn Paul Njeri.
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+/**
+ * Author: Kelyn Paul Njeri.
+ * Date: 29th March 2019.
+ */
+
+void main() => runApp(RandomWords());
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+	return new MaterialApp(
+		theme: ThemeData(
+			primaryColor: Colors.black,
+			accentColor: Colors.teal,
+		),
+		home: Scaffold (                    
+      appBar: new AppBar(
+        title: new Text('Name It'),
+      ),
+      body: _buildSuggestions(),
+    )  
+  );
+  }
+
+  Widget _buildSuggestions() {
+	/**
+	   * Generates a ListView of all the Two Word Pair Suggestions.
+	   */
+	return new ListView.builder(
+		padding: const EdgeInsets.all(16.0),
+		itemBuilder: (BuildContext _context, int i) {
+		  if (i.isOdd) {
+			return new Divider();
+		  }
+
+		  final int index = i ~/ 2;
+		  if (index >= _suggestions.length) {
+			_suggestions.addAll(generateWordPairs().take(10));
+		  }
+
+		  return _buildRow(_suggestions[index]);
+		});
+  }
+
+  Widget _buildRow(WordPair pair) {
+	return new ListTile(
+	  title: new Text(
+		pair.asPascalCase,
+		style: _biggerFont,
+	  ),
+	);
+  }
+}
